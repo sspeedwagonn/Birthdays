6d4323b0babe30ed97749249a923362921b7fc05
sspeedwagonn <burnerforcarter@gmail.com>
2026-04-06 16:35:09 -0700

    Trust trust trust the process

---
+++ src/main/java/net/civicraft/birthdays/BirthdayListener.java
@@ -0,0 +1,11 @@
+package net.civicraft.birthdays;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class BirthdayListener implements Listener {
+    @EventHandler
+    public void onBirthday() {
+
+    }
+}
