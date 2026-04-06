c4bffc0b5f19459eb28b84dee50242789378268c
sspeedwagonn <burnerforcarter@gmail.com>
2026-04-06 16:04:29 -0700

    initial files

---
+++ src/main/java/net/civicraft/birthdays/BirthdayCommand.java
@@ -0,0 +1,22 @@
+package net.civicraft.birthdays;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class BirthdayCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        if (commandSender instanceof Player player) {
+
+        } else {
+            commandSender.sendMessage(Component.text("You must be a player to use this command!", NamedTextColor.RED));
+            return false;
+        }
+    }
+}
---
+++ src/main/java/net/civicraft/birthdays/DataManager.java
@@ -0,0 +1,17 @@
+package net.civicraft.birthdays;
+
+import org.bukkit.entity.Player;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class DataManager {
+    public LocalDate getBirthdayByPlayer(Player player) {
+
+        return null;
+    }
+
+    public List<Player> getPlayersByBirthday(LocalDate date) {
+        return null;
+    }
+}
